0567f86594bc7e5987cc6a271c1dfe4b8f4c18db
Joohyung Park <joohyung.park@on-the-ground.org>
2025-03-06 14:38:34 +0900

    Modify TODOs

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 export default {
-  title: '',
-  author: '',
-  email: '',
+  title: 'Joohyung Park',
+  author: 'Joohyung Park',
+  email: 'joohyung.park@on-the-ground.org',
   description: '',
   keywords: [],
   language: 'en-US',
@@ -9,5 +9,5 @@ export default {
     widths: [32, 57, 76, 96, 128, 192, 228],
     format: 'png',
   },
-  url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'TODO:',
+  url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://joohyung-park.github.io',
 };
